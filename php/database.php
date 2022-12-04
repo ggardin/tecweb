bdf26f6e0fde613b74461a8da38df1de24bf9691
Riccardo Bonavigo <68009660+rickbonavigo@users.noreply.github.com>
2022-12-04 16:54:03 +0100

    php, database: - response->{bool, array} - prepared_select e prepared_insert con gestione eccezioni - altre piccole migliorie

--- php/database.php
+++ php/database.php
@@ -1,6 +1,7 @@
 <?php
 
-require_once("response.php");
+require_once("ini.php");
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
 class Database {
 	private const HOST = "mariadb";
@@ -8,43 +9,72 @@ class Database {
 	private const USER = "rbonavig";
 	private const PASS = "paJa5The1eiM4hei";
 
+	private const ERR = "Siamo spiacenti, abbiamo riscontrato un errore. Riprova più tardi.";
+
 	private $connection;
 
-	// see: https://phpdelusions.net/mysqli
+	// see: https://phpdelusions.net/mysqli/mysqli_connect
 	public function __construct() {
-		mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 		try {
 			$this->connection = new mysqli(self::HOST, self::USER, self::PASS, self::NAME);
 			$this->connection->set_charset("utf8mb4");
 			$this->connection->options(MYSQLI_OPT_INT_AND_FLOAT_NATIVE, 1);
-		} catch (\mysqli_sql_exception $e) { // to avoid leaking connection parameters
-			throw new \mysqli_sql_exception($e->getMessage(), $e->getCode());
+		} catch (mysqli_sql_exception $e) {
+			// throw new mysqli_sql_exception($e->getMessage(), $e->getCode()); // to avoid leaking connection parameters in the stack trace
+			throw new Exception(self::ERR);
 		}
 	}
 
 	public function __destruct() {
-		$this->connection->close();
+		if ($this->connection)
+			$this->connection->close();
 	}
 
+	// adattata da quella vista a lezione
 	private function pulisciInput(&$params) : void {
 		foreach ($params as &$p) {
 			if (is_string($p)) {
-				$p = trim($p); // elimina gli spazi
-				$p = strip_tags($p); // rimuove tag html (non sempre è una buona idea!)
-				$p = htmlentities($p); // converte i caratteri speciali in entità html (ex. &lt;)
+				$p = trim($p);
+				$p = strip_tags($p);
+				$p = htmlspecialchars($p);
 			}
 		}
 	}
 
 	// see: https://phpdelusions.net/mysqli
-	private function prepared_query($sql, &$params, $types = "") : mysqli_stmt|false {
+	private function prepared_query(&$query, &$params, $types = "") : mysqli_stmt {
 		$this->pulisciInput($params);
 		$types = $types ?: str_repeat("s", count($params));
-		$stmt = $this->connection->prepare($sql);
-		array_walk($params, "trim"); // NON FUNZIONA
+		$stmt = $this->connection->prepare($query);
 		$stmt->bind_param($types, ...$params);
 		$stmt->execute();
 		return $stmt;
+	}
+
+	private function prepared_select(&$query, &$params, $types = "") : array {
+		try {
+			$stmt = $this->prepared_query($query, $params, $types);
+			$result = $stmt->get_result();
+			$ret = $result->fetch_all(MYSQLI_ASSOC);
+			$result->close();
+			$stmt->close();
+			return $ret;
+		} catch (mysqli_sql_exception $e) {
+			throw new Exception(self::ERR);
+		}
+	}
+
+	private function prepared_insert(&$query, &$params, $types = "") : bool {
+		try {
+			$stmt = $this->prepared_query($query, $params, $types);
+			$stmt->close();
+		} catch (mysqli_sql_exception $e) {
+			if ($e->getCode() == 1062)
+				return false;
+			else
+				throw new Exception(self::ERR);
+		}
+		return true;
 	}
 
 	// see: https://phpdelusions.net/mysqli
@@ -54,34 +84,34 @@ class Database {
 		return array_filter($matches, "is_string", ARRAY_FILTER_USE_KEY);
 	}
 
-	public function getCollezioneById($id) : response {
+	public function getCollezioneById($id) : array {
 		$query = "select c.nome, f.titolo, f.data_rilascio
 			from collezione as c
 				join film as f on c.id = f.collezione
 			where c.id = ?";
 
 		$params = [$id];
-		$res = $this->prepared_query($query, $params)->get_result();
 
-		return new response(1, "", $res->fetch_all());
+		return $this->prepared_select($query, $params);
+
 		// TODO: altri casi
 	}
 
-	public function getPersoneByFilmId($id) : response {
-		$query = "SELECT fp.id, fp.persona, p.id, p.nome
+	public function getPersoneByFilmId($id) : array {
+		$query = "select fp.id, fp.persona, p.id, p.nome
 			from film as f
 				join film_partecipazione as fp on f.id=fp.film
 				join persona as p on p.id = fp.persona
 			where f.titolo = ?";
 
 		$params = [$id];
-		$res = $this->prepared_query($query, $params)->get_result();
 
-		return new response(1, "", $res->fetch_all());
+		return $this->prepared_select($query, $params);
+
 		// TODO: altri casi
 	}
 
-	public function getPersonaById($id) : response {
+	public function getPersonaById($id) : array {
 		$query = "select p.id, p.nome, f.id, f.titolo
 			from persona as p
 				join film_partecipazione as fp on p.id=fp.persona
@@ -89,40 +119,34 @@ class Database {
 			where p.id = ?";
 
 		$params = [$id];
-		$res = $this->prepared_query($query, $params)->get_result();
 
-		return new response(1, "", $res->fetch_all());
+		return $this->prepared_select($query, $params);
+
 		// TODO: altri casi
 	}
 
-	public function signup($user, $pass) : response {
+	public function signup($user, $pass) : bool {
 		$query = "insert into utente(username, password)
 			values (?, ?)";
 
 		$pass = password_hash($pass, PASSWORD_DEFAULT);
 		$params = [$user, $pass];
 
-		try {
-			$res = $this->prepared_query($query, $params);
-			return new response(1, "Registrato correttamente.");
-		} catch (\mysqli_sql_exception $e) {
-			return new response(0, "Utente già esistente.");
-		}
+		return $this->prepared_insert($query, $params);
 	}
 
-	public function login($user, $pass) : response {
+	public function login($user, $pass) : bool {
 		$query = "select password
 			from utente
 			where username=?";
 
 		$params = [$user];
-		$res = $this->prepared_query($query, $params)->get_result();
-		$user = $res->fetch_assoc();
+		$res = $this->prepared_select($query, $params);
 
-		if ($user && password_verify($pass, $user["password"]))
-			return new response(1, "Accesso eseguito.");
-		else
-			return new response(0, "Credenziali errate.");
+		$pw = !empty($res) ? $res[0]["password"] : null;
+		if ($pw && password_verify($pass, $pw))
+			return true;
+		return false;
 	}
 }
 
