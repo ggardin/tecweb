58b51c07dbb8848df2cdc6b2ace20a8ae37c9613
Riccardo Bonavigo <68009660+rickbonavigo@users.noreply.github.com>
2022-12-08 11:49:11 +0100

    {film,collezione}.php: casi errore e correzioni

--- collezione.php
+++ collezione.php
@@ -8,37 +8,48 @@ $page = Page::build(basename($_SERVER["PHP_SELF"], ".php"));
 $title = "Collezione";
 $content = "";
 
-if (isset($_GET["id"])) {
+$err = "Collezione non esistente";
+
+$id = (isset($_GET["id"])) ? $_GET["id"] : "";
+
+if ($id != "") {
 	$db_ok = false;
 	try {
 		$connessione = new Database();
-		$collezione = $connessione->getCollezioneById($_GET["id"])[0];
-		$film = $connessione->getFilmInCollezioneById($_GET["id"]);
+		$collezione = $connessione->getCollezioneById($id);
+		if (!empty($collezione)) {
+			$collezione = $collezione[0];
+			$film = $connessione->getFilmInCollezioneById($id);
+		}
 		$db_ok = true;
 	} catch (Exception $e) {
-		$content = "<h2>" . $e->getMessage() . "</h2>";
+		$content .= "<h1>" . $e->getMessage() . "</h1>";
 	} finally {
 		unset($connessione);
 	}
 	if ($db_ok) {
-		$title = Page::langToNone($collezione["nome"]) . " - " . $title;
-		$collezione["nome"] = Page::langToSpan($collezione["nome"]);
-		$content = "<h1>" . $collezione["nome"] . "</h1>";
-		$content .= '<img height="300" size="200" src="https://www.themoviedb.org/t/p/w500/' . $collezione["copertina"] . '" alt="copertina" />';
-		$content .= '<p>Descrizione: ' . $collezione["descrizione"] . '</p>';
-		$content .= '<p>film: </p><ol>';
-		foreach ($film as $f) {
-			$f["nome"] = Page::langToSpan($f["nome"]);
-			$content .= '<li><ul>';
-				$content .= '<li>Copertina: ' . '<img height="150" size="100" src="https://www.themoviedb.org/t/p/w500/' . $f["copertina"] . '" alt="copertina" />' . '</li>';
-				$content .= '<li>Link: <a href="film.php?id=' . $f["id"] . '">' . $f["nome"] . '</a></li>';
-				$content .= '<li>Data rilascio: ' . $f["data_rilascio"] . '</li>';
-			$content .= '</ul></li>';
+		if (!empty($collezione)) {
+			$title = Page::langToTag($collezione["nome"], "") . " -- " . $title;
+			$content .= "<h1>" . Page::langToTag($collezione["nome"]) . "</h1>";
+			$content .= '<img height="300" size="200" src="https://www.themoviedb.org/t/p/w500/' . $collezione["copertina"] . '" alt="copertina" />';
+			$content .= "<p>Descrizione: " . Page::langToTag($collezione["descrizione"]) . "</p>";
+			$content .= "<p>film: </p>";
+
+			$content .= "<ol>";
+			foreach ($film as $f) {
+				$content .= "<li><ul>";
+					$content .= '<li>Copertina: ' . '<img height="150" size="100" src="https://www.themoviedb.org/t/p/w500/' . $f["copertina"] . '" alt="copertina" />' . '</li>';
+					$content .= '<li>Link: <a href="film.php?id=' . $f["id"] . '">' . Page::langToTag($f["nome"]) . '</a></li>';
+					$content .= '<li>Data rilascio: ' . $f["data_rilascio"] . '</li>';
+				$content .= "</ul></li>";
+			}
+			$content .= "</ol>";
+		} else {
+			$content .= "<h1>" . $err . "</h1>";
 		}
-		$content .= '</ol>';
 	}
 } else {
-	$content = "<p>¯\_(ツ)_/¯</p>";
+	$content .= "<h1>" . $err . "</h1>";
 }
 
 Page::replaceAnchor($page, "title", $title);
