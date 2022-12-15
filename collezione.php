58b5903a0a2ee4a742e7e3a78ca93352b7e7a141
Riccardo Bonavigo <68009660+rickbonavigo@users.noreply.github.com>
2022-12-15 22:24:11 +0100

    tolti isset inutili

--- collezione.php
+++ collezione.php
@@ -33,15 +33,15 @@ if ($id != "") {
 			Tools::replaceAnchor($page, "title", Tools::langToTag($collezione["nome"], "") . " · Collezione");
 			Tools::replaceAnchor($page, "breadcrumb", Tools::langToTag($collezione["nome"]));
 			$content .= "<h1>" . Tools::langToTag($collezione["nome"]) . "</h1>";
-			$content .= '<img width="250" height="375" src="' . (isset($collezione["locandina"]) ? ("https://www.themoviedb.org/t/p/w300/" . $collezione["locandina"]) : "img/placeholder.svg") . '" alt="" />';
-			if (isset($collezione["descrizione"]))
+			$content .= '<img width="250" height="375" src="' . ($collezione["locandina"] ? ("https://www.themoviedb.org/t/p/w300/" . $collezione["locandina"]) : "img/placeholder.svg") . '" alt="" />';
+			if ($collezione["descrizione"])
 				$content .= "<p>Descrizione: " . Tools::langToTag($collezione["descrizione"]) . "</p>";
 			$content .= "<p>film: </p>";
 
 			$content .= "<ol>";
 			foreach ($film as $f) {
 				$content .= "<li><ul>";
-					$content .= '<img width="250" height="375" src="' . (isset($f["locandina"]) ? ("https://www.themoviedb.org/t/p/w300/" . $f["locandina"]) : "img/placeholder.svg") . '" alt="" />';
+					$content .= '<img width="250" height="375" src="' . ($f["locandina"] ? ("https://www.themoviedb.org/t/p/w300/" . $f["locandina"]) : "img/placeholder.svg") . '" alt="" />';
 					$content .= '<li>Link: <a href="film.php?id=' . $f["id"] . '">' . Tools::langToTag($f["nome"]) . '</a></li>';
 					$content .= '<li>Data rilascio: ' . $f["data_rilascio"] . '</li>';
 				$content .= "</ul></li>";
