70157e10b48d18b4d467677fcc2ae3c97f135dab
Riccardo Bonavigo <68009660+rickbonavigo@users.noreply.github.com>
2022-12-20 13:01:26 +0100

    php: riaggiunti isset dove è bene ci siano (#112)
    * php: riaggiunti isset dove è bene ci siano
    
    * php: riaggiunti isset pt.2

--- persona.php
+++ persona.php
@@ -33,17 +33,17 @@ if ($id != "") {
 			Tools::replaceAnchor($page, "title", Tools::stripSpanLang($persona["nome"]) . " · Persona");
 			Tools::replaceAnchor($page, "breadcrumb", $persona["nome"]);
 			$content .= "<h1>" . $persona["nome"] . "</h1>";
-			$content .= '<img width="250" height="375" src="' . ($persona["immagine"] ? ("https://www.themoviedb.org/t/p/w300/" . $persona["immagine"]) : "img/placeholder.svg") . '" alt="" />';
+			$content .= '<img width="250" height="375" src="' . (isset($persona["immagine"]) ? ("https://www.themoviedb.org/t/p/w300/" . $persona["immagine"]) : "img/placeholder.svg") . '" alt="" />';
 			$content .= '<p><span lang="en">Gender</span>: ' . $persona["gender"] . "</p>";
-			if ($persona["data_nascita"])
+			if (isset($persona["data_nascita"]))
 				$content .= "<p>Data nascita: " . $persona["data_nascita"] . "</p>";
-			if ($persona["data_morte"])
+			if (isset($persona["data_morte"]))
 				$content .= "<p>Data morte: " . $persona["data_morte"] . "</p>";
 			$content .= "<p>film: </p>";
 			$content .= "<ol>";
 			foreach ($film as $f) {
 				$content .= "<li><ul>";
-					$content .= '<img width="250" height="375" src="' . ($f["locandina"] ? ("https://www.themoviedb.org/t/p/w300/" . $f["locandina"]) : "img/placeholder.svg") . '" alt="" />';
+					$content .= '<img width="250" height="375" src="' . (isset($f["locandina"]) ? ("https://www.themoviedb.org/t/p/w300/" . $f["locandina"]) : "img/placeholder.svg") . '" alt="" />';
 					$content .= '<li>Link: <a href="film.php?id=' . $f["id"] . '">' . $f["nome"] . '</a></li>';
 					$content .= '<li>Ruolo: ' . $f["ruolo"] . '</li>';
 					$content .= '<li>Data rilascio: ' . $f["data_rilascio"] . '</li>';
