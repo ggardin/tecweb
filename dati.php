8c9303c7ca7127d6d21db6399a12fdaa62ad3ec6
Riccardo Bonavigo <68009660+rickbonavigo@users.noreply.github.com>
2022-12-21 00:21:55 +0100

    php: redirect a login se non loggato per pagine utente

--- dati.php
+++ dati.php
@@ -2,6 +2,13 @@
 
 require_once("php/tools.php");
 
+session_start();
+
+if (! isset($_SESSION["user_id"])) {
+	header ("location: login.php");
+	exit();
+}
+
 $page = Tools::buildPage(basename($_SERVER["PHP_SELF"], ".php"));
 
 Tools::showPage($page);
