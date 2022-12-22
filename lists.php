a649c6fd1a4d13355f3486c8d2cbdb8ca88a5819
Riccardo Bonavigo <68009660+rickbonavigo@users.noreply.github.com>
2022-12-22 01:27:38 +0100

    lists.php: prima integrazione

--- lists.php
+++ lists.php
@@ -1,15 +1,44 @@
 <?php
 
 require_once("php/tools.php");
+require_once("php/database.php");
 
 session_start();
 
-if (! isset($_SESSION["user_id"])) {
+if (! isset($_SESSION["id"])) {
 	header ("location: login.php");
 	exit();
 }
 
 $page = Tools::buildPage(basename($_SERVER["PHP_SELF"], ".php"));
+
+$db_ok = false;
+try {
+	$connessione = new Database();
+	$liste = $connessione->getListsByUserId($_SESSION["id"]);
+	$db_ok = true;
+} catch (Exception $e) {
+	Tools::replaceSection($page, "message", $e->getMessage());
+} finally {
+	unset($connessione);
+}
+if ($db_ok) {
+	if (!empty($liste)) {
+		$lista = Tools::getSection($page, "lista");
+		$r = "";
+		foreach ($liste as $l) {
+			$t = $lista;
+			Tools::replaceAnchor($t, "id", $l["id"]);
+			Tools::replaceAnchor($t, "nome", $l["nome"]);
+			$r .= $t;
+		}
+		Tools::replaceSection($page, "lista", $r);
+		Tools::replaceAnchor($page, "message", ("Ne hai " . count($liste)));
+	} else {
+		Tools::replaceAnchor($page, "message", "Inizia creandone una.");
+		Tools::replaceSection($page, "liste", "");
+	}
+}
 
 Tools::showPage($page);
 
