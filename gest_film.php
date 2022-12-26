c88be52f3de625e98110963a22623f10e65bda16
Riccardo Bonavigo <68009660+rickbonavigo@users.noreply.github.com>
2022-12-26 18:53:59 +0100

    gest_film.php: controllo sessione e scaletta

--- gest_film.php
+++ gest_film.php
@@ -1,8 +1,27 @@
 <?php
 
 require_once("php/tools.php");
+require_once("php/database.php");
 
-$page = Tools::buildPage(basename($_SERVER["PHP_SELF"], ".php"), "std", true);
+session_start();
+
+if (! isset($_SESSION["id"]) || $_SESSION["is_admin"] == 0) {
+	header ("location: user.php");
+	exit();
+}
+
+$page = Tools::buildPage(basename($_SERVER["PHP_SELF"], ".php"));
+
+/*
+if post id :
+	breadcrumb: film > nome > modifica
+	dati: compila
+	button: modifica
+else :
+	breadcrumb: film > aggiungi
+	button: aggiungi
+
+*/
 
 Tools::showPage($page);
 
