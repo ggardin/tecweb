fa24f183184c8db94d678fe196fcadc76d760cb7
Riccardo Bonavigo <68009660+rickbonavigo@users.noreply.github.com>
2022-12-27 01:03:41 +0100

    gest_film.php: prima versione che mostra i dati di film presente

--- gest_film.php
+++ gest_film.php
@@ -10,19 +10,42 @@ if (! isset($_SESSION["id"]) || $_SESSION["is_admin"] == 0) {
 	exit();
 }
 
-$page = Tools::buildPage(basename($_SERVER["PHP_SELF"], ".php"));
+if (isset($_GET["id"])) $id = $_GET["id"];
 
-/*
-if post id :
-	breadcrumb: film > nome > modifica
-	dati: compila
-	button: modifica
-else :
-	breadcrumb: film > aggiungi
-	button: aggiungi
-
-*/
-
-Tools::showPage($page);
+$db_ok = false;
+try {
+	$connessione = new Database();
+	if (isset($id)) $film = $connessione->getFilmById($id);
+	unset($connessione);
+	$db_ok = true;
+} catch (Exception) {
+	unset($connessione);
+	Tools::errCode(500);
+}
+if ($db_ok) {
+	$page = Tools::buildPage(basename($_SERVER["PHP_SELF"], ".php"));
+	if (isset($id) && !empty($film)) {
+		$film = $film[0];
+		$title = $film["nome"] . " • Modifica Film"; Tools::toHtml($title, 0);
+		Tools::replaceAnchor($page, "title", $title);
+		Tools::replaceAnchor($page, "bc_id", $id);
+		$bc_nome = $film["nome"]; Tools::toHtml($bc_nome, 2);
+		Tools::replaceAnchor($page, "bc_nome", $bc_nome);
+		Tools::toHtml($film, 1);
+		Tools::replaceAnchor($page, "nome", $film["nome"]);
+		Tools::replaceAnchor($page, "descrizione", $film["descrizione"]);
+		Tools::replaceAnchor($page, "data_rilascio", $film["data_rilascio"]);
+		Tools::replaceAnchor($page, "durata", $film["durata"]);
+		Tools::replaceAnchor($page, "incassi", $film["incassi"]);
+		Tools::replaceAnchor($page, "budget", $film["budget"]);
+		Tools::replaceAnchor($page, "nome_originale", $film["nome_originale"]);
+		Tools::replaceAnchor($page, "submit", "Modifica");
+	} else {
+		Tools::replaceAnchor($page, "title", "Aggiungi Film");
+		Tools::replaceSection($page, "breadcrumb", "Aggiungi");
+		Tools::replaceAnchor($page, "submit", "Aggiungi");
+	}
+	Tools::showPage($page);
+}
 
 ?>
