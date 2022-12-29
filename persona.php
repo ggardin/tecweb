17cee3c786b8b07f4f1543325a02160720fc4aa9
Riccardo Bonavigo <68009660+rickbonavigo@users.noreply.github.com>
2022-12-29 23:07:45 +0100

    persona.php: integrazione con scheletro

--- persona.php
+++ persona.php
@@ -9,58 +9,79 @@ else {
 	exit();
 }
 
-$content = "";
-$page = Tools::buildPage(basename($_SERVER["PHP_SELF"], ".php"));
-
-// so che è da cambiare l'indentazione, ma è da spostare comunque tutto in html
-	$db_ok = false;
-	try {
-		$connessione = new Database();
-		$persona = $connessione->getPersonaById($id);
-		if (!empty($persona)) {
-			$film = $connessione->getFilmByPersonaId($id);
-		}
-		unset($connessione);
-		$db_ok = true;
-	} catch (Exception) {
-		unset($connessione);
-		Tools::errCode(500);
+$db_ok = false;
+try {
+	$connessione = new Database();
+	$persona = $connessione->getPersonaById($id);
+	if (!empty($persona)) {
+		$film = $connessione->getFilmByPersonaId($id);
+	}
+	unset($connessione);
+	$db_ok = true;
+} catch (Exception) {
+	unset($connessione);
+	Tools::errCode(500);
+	exit();
+}
+if ($db_ok) {
+	if (empty($persona)) {
+		Tools::errCode(404);
 		exit();
 	}
-	if ($db_ok) {
-		if (!empty($persona)) {
-			$persona = $persona[0];
-			$title = $persona["nome"] . " • Persona"; Tools::toHtml($title, 0);
-			Tools::replaceAnchor($page, "title", $title);
-			Tools::toHtml($persona);
-			Tools::toHtml($film);
-			Tools::replaceAnchor($page, "breadcrumb", $persona["nome"]);
-			$content .= "<h1>" . $persona["nome"] . "</h1>";
-			$content .= '<img width="250" height="375" src="' . (isset($persona["immagine"]) ? ("https://www.themoviedb.org/t/p/w300/" . $persona["immagine"]) : "img/placeholder.svg") . '" alt="" />';
-			$content .= '<p><span lang="en">Gender</span>: ' . $persona["gender"] . "</p>";
-			if (isset($persona["data_nascita"]))
-				$content .= "<p>Data nascita: " . $persona["data_nascita"] . "</p>";
-			if (isset($persona["data_morte"]))
-				$content .= "<p>Data morte: " . $persona["data_morte"] . "</p>";
-			$content .= "<p>film: </p>";
-			$content .= "<ol>";
-			foreach ($film as $f) {
-				$content .= "<li><ul>";
-					$content .= '<img width="250" height="375" src="' . (isset($f["locandina"]) ? ("https://www.themoviedb.org/t/p/w300/" . $f["locandina"]) : "img/placeholder.svg") . '" alt="" />';
-					$content .= '<li>Link: <a href="film.php?id=' . $f["id"] . '">' . $f["nome"] . '</a></li>';
-					$content .= '<li>Ruolo: ' . $f["ruolo"] . '</li>';
-					$content .= '<li>Data rilascio: ' . $f["data_rilascio"] . '</li>';
-				$content .= "</ul></li>";
+	// else
+	$page = Tools::buildPage(basename($_SERVER["PHP_SELF"], ".php"));
+	$persona = $persona[0];
+	$title = $persona["nome"] . " • Persona"; Tools::toHtml($title, 0);
+	Tools::replaceAnchor($page, "title", $title);
+	Tools::toHtml($persona);
+	Tools::replaceAnchor($page, "breadcrumb", $persona["nome"]);
+	Tools::replaceAnchor($page, "nome", $persona["nome"]);
+	$immagine = (isset($persona["immagine"]) ? ("https://www.themoviedb.org/t/p/w300/" . $persona["immagine"]) : "img/placeholder.svg");
+	Tools::replaceAnchor($page, "immagine", $immagine);
+	if (isset($persona["data_nascita"]))
+		Tools::replaceAnchor($page, "data_nascita", $persona["data_nascita"]);
+	else
+		Tools::replaceSection($page, "data_nascita", "");
+	if (isset($persona["data_morte"]))
+		Tools::replaceAnchor($page, "data_morte", $persona["data_morte"]);
+	else
+		Tools::replaceSection($page, "data_morte", "");
+	Tools::replaceAnchor($page, "gender", $persona["gender"]);
+	if (!empty($film)) {
+		Tools::toHtml($film);
+		$card = Tools::getSection($page, "card");
+		$ruolo = Tools::getSection($page, "ruolo");
+		$res = "";
+		$last_film_id = -1;
+		$last_film = "";
+		$last_film_ruoli = "";
+		foreach ($film as $f) {
+			if ($f["id"] != $last_film_id) {
+				Tools::replaceSection($last_film, "ruolo", $last_film_ruoli);
+				$res .= $last_film;
+				$c = $card;
+				Tools::replaceAnchor($c, "id", $f["id"]);
+				$immagine = (isset($f["locandina"]) ? ("https://www.themoviedb.org/t/p/w300/" . $f["locandina"]) : "img/placeholder.svg");
+				Tools::replaceAnchor($c, "immagine", $immagine);
+				Tools::replaceAnchor($c, "nome", $f["nome"]);
+				if (isset($f["data_rilascio"]))
+					Tools::replaceAnchor($c, "data_rilascio", $f["data_rilascio"]);
+				else
+					Tools::replaceSection($c, "data_rilascio", "");
+				$last_film = $c;
+				$last_film_ruoli = "";
+				$last_film_id = $f["id"];
 			}
-			$content .= "</ol>";
-		} else {
-			Tools::errCode(404);
-			exit();
+			$r = $ruolo;
+			Tools::replaceAnchor($r, "ruolo", $f["ruolo"]);
+			$last_film_ruoli .= $r;
 		}
-	}
-
-Tools::replaceAnchor($page, "persona", $content);
-
-Tools::showPage($page);
+		Tools::replaceSection($last_film, "ruolo", $last_film_ruoli);
+		$res .= $last_film;
+		Tools::replaceSection($page, "card", $res);
+	} else
+		Tools::replaceSection($page, "partecipazioni", "");
+	Tools::showPage($page);
+}
 
 ?>
