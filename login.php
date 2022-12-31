e17bcf82fabf2f6d3f16a3cad683999d8c1caad7
Riccardo Bonavigo <68009660+rickbonavigo@users.noreply.github.com>
2022-12-31 13:09:57 +0100

    login: usa `post_login.php`, bozza per accesso

--- login.php
+++ login.php
@@ -9,33 +9,7 @@ if (isset($_SESSION["id"])) {
 	exit();
 }
 
-$username = isset($_POST["username"]) ? $_POST["username"] : "";
-$password = isset($_POST["password"]) ? $_POST["password"] : "";
-
 $page = Tools::buildPage(basename($_SERVER["PHP_SELF"], ".php"), "auth");
-
-if (isset($_POST["submit"])) {
-	try {
-		$connessione = new Database();
-		$res = $connessione->login($username, $password);
-		unset($connessione);
-	} catch (Exception) {
-		unset($connessione);
-		Tools::errCode(500);
-		exit();
-	}
-	if (! empty($res)) {
-		$_SESSION["id"] = $res["id"];
-		$_SESSION["is_admin"] = $res["is_admin"];
-		header("location: user.php");
-		exit();
-	} else
-		Tools::replaceAnchor($page, "message", "Errore: Credenziali errate");
-} else
-	Tools::replaceSection($page, "message", "");
-
-Tools::replaceAnchor($page, "form_username", $username);
-Tools::replaceAnchor($page, "form_password", $password);
 
 Tools::showPage($page);
 
