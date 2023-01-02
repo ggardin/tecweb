c00d0c3144c9feaa0eee6630986c38978530f9a6
Riccardo Bonavigo <68009660+rickbonavigo@users.noreply.github.com>
2023-01-02 17:48:31 +0100

    aggiunta bozza gest_persona.{html,php}

---
+++ gest_persona.php
@@ -0,0 +1,48 @@
+<?php
+
+require_once("php/tools.php");
+require_once("php/database.php");
+
+session_start();
+
+if (! isset($_SESSION["id"]) || $_SESSION["is_admin"] == 0) {
+	header ("location: user.php");
+	exit();
+}
+
+$id = isset($_GET["id"]) ? $_GET["id"] : "";
+
+try {
+	$connessione = new Database();
+	if (isset($id))
+		$persona = $connessione->getPersonaById($id);
+	unset($connessione);
+} catch (Exception) {
+	unset($connessione);
+	Tools::errCode(500);
+	exit();
+}
+
+$page = Tools::buildPage(basename($_SERVER["PHP_SELF"], ".php"));
+
+if ($id != "" && !empty($persona)) {
+	$persona = $persona[0];
+	$title = $persona["nome"] . " • Modifica Persona"; Tools::toHtml($title, 0);
+	Tools::replaceAnchor($page, "title", $title);
+	Tools::replaceAnchor($page, "bc_id", $id);
+	$bc_nome = $persona["nome"]; Tools::toHtml($bc_nome, 2);
+	Tools::replaceAnchor($page, "bc_nome", $bc_nome);
+	Tools::toHtml($persona, 1);
+	Tools::replaceAnchor($page, "nome", $persona["nome"]);
+	Tools::replaceAnchor($page, "data_nascita", $persona["data_nascita"]);
+	Tools::replaceAnchor($page, "data_morte", $persona["data_morte"]);
+	Tools::replaceAnchor($page, "submit", "Modifica");
+} else {
+	Tools::replaceAnchor($page, "title", "Aggiungi Persona");
+	Tools::replaceSection($page, "breadcrumb", "Aggiungi");
+	Tools::replaceAnchor($page, "submit", "Aggiungi");
+}
+
+Tools::showPage($page);
+
+?>
