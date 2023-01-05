4f7c6d27ccde22cf106c36482a6e01535b84779f
Riccardo Bonavigo <68009660+rickbonavigo@users.noreply.github.com>
2023-01-05 01:04:44 +0100

    php: altro cambio nome funzione database.php

--- php/database.php
+++ php/database.php
@@ -450,7 +450,7 @@ class Database {
 		return $this->preparedUpdates($query, $params, $types);
 	}
 
-	public function puoValutare($user_id, $film_id) : bool {
+	public function canValutare($user_id, $film_id) : bool {
 		$query = "select *
 			from valutazione
 			where utente = ? and film = ?";
