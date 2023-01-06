6d0bdc67059f1a0587bd0245f003e03e5127eb01
Riccardo Bonavigo <68009660+rickbonavigo@users.noreply.github.com>
2023-01-06 02:21:19 +0100

    404 su cerca.php se non c'è tipo settato

--- cerca.php
+++ cerca.php
@@ -3,6 +3,11 @@
 require_once("php/tools.php");
 require_once("php/database.php");
 
+if (! isset($tipo)) {
+	Tools::errCode("404");
+	exit();
+}
+
 $query = (isset($_GET["q"])) ? $_GET["q"] : "";
 // $tipo = (isset($_GET["t"])) ? $_GET["t"] : "film";
 $f_nome = (isset($_GET["fn"])) ? $_GET["fn"] : "";
