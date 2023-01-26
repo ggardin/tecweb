9fbbd67df454d70668d50a52bd75569f0d5d990c
Riccardo Bonavigo <68009660+rickbonavigo@users.noreply.github.com>
2023-01-26 11:49:56 +0100

    cerca: tolto skip per filtri di ricerca

--- cerca.php
+++ cerca.php
@@ -137,10 +137,8 @@ if ($tipo == "film") {
 		$res .= $t;
 	}
 	Tools::replaceSection($page, "paese", $res);
-}  else {
+} else
 	Tools::replaceSection($page, "filtri", "");
-	Tools::replaceSection($page, "filtrisr", "");
-}
 
 if (!empty($cerca[0])) {
 	$tot = $cerca[1]["n"];
