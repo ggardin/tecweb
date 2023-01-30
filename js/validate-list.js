247e2769e169ea1fabf75adaed6975ff98e4ccd0
Giovanni <2914344+ggardin@users.noreply.github.com>
2023-01-30 18:38:23 +0100

    Refuse curly brackets
    Fix #405

--- js/validate-list.js
+++ js/validate-list.js
@@ -44,7 +44,7 @@ function validateListDescription() {
 	var description = document.forms['gestione'][id].value;
 
 	if (description != null || description != '') {
-		const descriptionRegex = /^[^<>]*$/;
+		const descriptionRegex = /^[^<>{}]*$/;
 		if (! descriptionRegex.test(description)) {
 			showErrorMessage(id, 'La descrizione inserita contiene caratteri non ammessi.');
 			return false;
