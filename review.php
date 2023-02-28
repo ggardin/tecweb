8643ff178101ae4d5056e5590b316733f4b9cc0a
Giovanni <2914344+ggardin@users.noreply.github.com>
2023-02-28 08:47:13 +0100

    Convert indentation to tabs

--- review.php
+++ review.php
@@ -10,7 +10,7 @@ if (! isset($_SESSION["id"])) {
 
 try {
 	$connessione = new Database();
-    $valutazione = $connessione->getValutazioniPerUtente($_SESSION["id"]);
+	$valutazione = $connessione->getValutazioniPerUtente($_SESSION["id"]);
 	unset($connessione);
 } catch (Exception) {
 	unset($connessione);
@@ -27,15 +27,15 @@ if (!empty($valutazione)) {
 	$r = "";
 	foreach ($valutazione as $v) {
 		$t = $list;
-        Tools::replaceAnchor($t, "titolo", $v["nome"]);
-        Tools::replaceAnchor($t, "id", $v["id"]);
+		Tools::replaceAnchor($t, "titolo", $v["nome"]);
+		Tools::replaceAnchor($t, "id", $v["id"]);
 		Tools::replaceAnchor($t, "voto", $v["voto"]);
 		Tools::replaceAnchor($t, "testo", $v["testo"]);
 		$r .= $t;
 	}
 	Tools::replaceSection($page, "valutazione", $r);
 } else {
-    Tools::replaceAnchor($page, "message", "Non hai ancora lasciato una valutazione, vai alla pagina di un film per lasciarne una.");
+	Tools::replaceAnchor($page, "message", "Non hai ancora lasciato una valutazione, vai alla pagina di un film per lasciarne una.");
 	Tools::replaceSection($page, "valutazioni", "");
 }
 
